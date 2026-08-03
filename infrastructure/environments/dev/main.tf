54869b32af134c96b9ce7823bcf3848ae79d6184
Mir Balach <balach25@gmail.com>
2026-08-03 14:14:29 +0500

    Add Terraform module for Amazon ECR repository

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -4,3 +4,11 @@ module "terraform_state_bucket" {
   bucket_name = "mbalach-cloud-native-platform-2026-tfstate"
   environment = "dev"
 }
+
+
+module "application_repository" {
+  source = "../../modules/ecr"
+
+  repository_name = "cloud-native-app"
+  environment     = "dev"
+}
