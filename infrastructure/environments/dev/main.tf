a198ef55909eb575d3b4aeda9606897f8dfb8582
Mir Balach <balach25@gmail.com>
2026-08-03 13:53:17 +0500

    Add reusable Terraform module for secure S3 bucket

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,1 +1,6 @@
-# Infrastructure resources will be added here in the next step.
+module "terraform_state_bucket" {
+  source = "../../modules/s3"
+
+  bucket_name = "mbalach-cloud-native-platform-2026-tfstate"
+  environment = "dev"
+}
