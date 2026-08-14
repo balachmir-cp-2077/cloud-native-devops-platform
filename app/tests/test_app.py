e8cb1bf7d25238c46b5cd6cb39dc253761a1aee1
Mir Balach <balach25@gmail.com>
2026-08-14 17:19:13 +0500

    Add automated application tests

---
+++ app/tests/test_app.py
@@ -0,0 +1,26 @@
+from src.app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["application"] == "Cloud Native DevOps Platform"
+    assert data["status"] == "Running"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
